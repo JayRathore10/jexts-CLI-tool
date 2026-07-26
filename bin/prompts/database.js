5927ca16350abb814465316c9d52260b2fae6ad7
Jay <jayrathore88155@gmail.com>
2026-07-26 21:26:59 +0530

    feat: Add database.js

---
+++ bin/prompts/database.js
@@ -0,0 +1,30 @@
+import prompts from "prompts";
+
+export async function promptDatabase() {
+  const response = await prompts({
+    type: "select",
+    name: "database",
+    message: "Database",
+    initial: 0,
+    choices: [
+      {
+        title: "None",
+        value: "none"
+      },
+      {
+        title: "MongoDB",
+        value: "mongodb"
+      },
+      {
+        title: "PostgreSQL",
+        value: "postgresql"
+      },
+      {
+        title: "MySQL",
+        value: "mysql"
+      }
+    ]
+  });
+
+  return response.database;
+}
