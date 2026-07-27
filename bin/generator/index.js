10bda387eae24f79bb4c5162788d5999f4e51672
Jay <jayrathore88155@gmail.com>
2026-07-27 15:51:03 +0530

    refactor: improve project generation pipeline

--- bin/generator/index.js
+++ bin/generator/index.js
@@ -1,29 +1,118 @@
-import { createProject } from "./createProject.js";
-import { copyTemplate } from "./copyTemplate.js";
+import fs from "fs-extra";
+import path from "path";
+
 import { setupFeatures } from "./features/index.js";
 import { updatePackageJson } from "./packageJson.js";
-import { configureGitignore } from "./gitignore.js";
-import { installDependencies } from "./installDependencies.js";
-import { initializeGit } from "./initializeGit.js";
 import { createEnvFile } from "./env.js";
 import { updateServer } from "./updateServer.js";
+import { configureGitignore } from "./gitignore.js";
+import { initializeGit } from "./git.js";
+import { installDependencies } from "./install.js";
+import { ROOT_DIR } from "../utils/paths.js";
+
+
 
 export async function generateProject(config) {
-  await createProject(config);
 
-  await copyTemplate(config);
 
-  await setupFeatures(config);
+  const targetDir = path.join(
+    process.cwd(),
+    config.projectName
+  );
 
-  await updateServer(config);
 
-  await updatePackageJson(config);
+  config.targetDir = targetDir;
 
-  await createEnvFile(config);
 
-  await configureGitignore(config);
 
-  await installDependencies(config);
+  await createProjectDirectory(
+    targetDir
+  );
 
-  await initializeGit(config);
+
+
+  await copyTemplate(
+    config
+  );
+
+
+
+  await setupFeatures(
+    config
+  );
+
+
+
+  await updatePackageJson(
+    config
+  );
+
+
+
+  await createEnvFile(
+    config
+  );
+
+
+
+  await updateServer(
+    config
+  );
+
+
+
+  await configureGitignore(
+    config
+  );
+
+
+
+  if (config.git) {
+
+    await initializeGit(
+      config
+    );
+
+  }
+
+
+
+  await installDependencies(
+    config
+  );
+
+
+}
+
+
+
+async function createProjectDirectory(
+  targetDir
+) {
+
+  await fs.mkdirp(
+    targetDir
+  );
+
+}
+
+
+
+async function copyTemplate(
+  config
+) {
+
+  const templateDir =
+    path.join(
+      ROOT_DIR,
+      "templates",
+      config.language
+    );
+
+
+  await fs.copy(
+    templateDir,
+    config.targetDir
+  );
+
 }
---
+++ bin/utils/path.js
@@ -0,0 +1,17 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename =
+fileURLToPath(import.meta.url);
+
+
+const __dirname =
+path.dirname(__filename);
+
+
+export const ROOT_DIR =
+path.join(
+  __dirname,
+  "../.."
+);
