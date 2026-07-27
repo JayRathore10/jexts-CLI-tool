c6f31233523ae2723d5408e5992e3e51153f74aa
Jay <jayrathore88155@gmail.com>
2026-07-27 15:47:46 +0530

    refactor: improve feature registry architecture

---
+++ bin/generator/features/registry.js
@@ -0,0 +1,36 @@
+import { setupDatabase } from "./database.js";
+import { setupORM } from "./orm.js";
+import { setupAuthentication } from "./authentication.js";
+import { setupTesting } from "./testing.js";
+import { setupESLint } from "./eslint.js";
+import { setupPrettier } from "./prettier.js";
+import { setupHusky } from "./husky.js";
+import { setupDocker } from "./docker.js";
+import { setupSwagger } from "./swagger.js";
+import { setupExampleApi } from "./exampleApi.js";
+
+
+const features = [
+  setupDatabase,
+  setupORM,
+  setupAuthentication,
+  setupTesting,
+  setupESLint,
+  setupPrettier,
+  setupHusky,
+  setupDocker,
+  setupSwagger,
+  setupExampleApi
+];
+
+
+
+export async function setupFeatures(config) {
+
+  for (const feature of features) {
+
+    await feature(config);
+
+  }
+
+}
