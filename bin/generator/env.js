3104802c6a736a488fbb12aa20ff908ee6196f5c
Jay <jayrathore88155@gmail.com>
2026-08-03 11:55:15 +0530

    fix: env.js

--- bin/generator/env.js
+++ bin/generator/env.js
@@ -2,66 +2,30 @@ import fs from "fs-extra";
 import path from "path";
 import { createSpinner } from "../utils/ui.js";
 
-
 export async function createEnvFile(config) {
-
-
-  if (
-    !config.env ||
-    Object.keys(config.env).length === 0
-  ) {
+  if (!config.env || Object.keys(config.env).length === 0) {
     return;
   }
 
-
-  const spinner = createSpinner(
-    "Creating environment file"
-  ).start();
-
+  const spinner = createSpinner("Creating environment file").start();
 
   try {
-
-
     const envPath = path.join(
       config.targetDir,
       ".env.example"
     );
 
-
     let content = "";
 
-
-    for (
-      const [key, value]
-      of Object.entries(config.env)
-    ) {
-
+    for (const [key, value] of Object.entries(config.env)) {
       content += `${key}=${value}\n`;
-
     }
 
+    await fs.writeFile(envPath, content);
 
-    await fs.writeFile(
-      envPath,
-      content
-    );
-
-
-    spinner.succeed(
-      ".env.example created"
-    );
-
-
-  } catch(error) {
-
-
-    spinner.fail(
-      "Failed to create environment file"
-    );
-
-
+    spinner.succeed(".env.example created");
+  } catch (error) {
+    spinner.fail("Failed to create environment file");
     throw error;
-
   }
-
 }
