10999c4c105438fe99a2067b64909bed4d9108e2
Jay <jayrathore88155@gmail.com>
2026-08-03 11:57:12 +0530

    fix: packagejson.js

--- bin/generator/packageJson.js
+++ bin/generator/packageJson.js
@@ -2,95 +2,38 @@ import fs from "fs-extra";
 import path from "path";
 import { createSpinner } from "../utils/ui.js";
 
-
 export async function updatePackageJson(config) {
-
-
-  const spinner = createSpinner(
-    "Updating package.json"
-  ).start();
-
+  const spinner = createSpinner("Updating package.json").start();
 
   try {
+    const packageJsonPath = path.join(
+      config.targetDir,
+      "package.json"
+    );
 
-
-    const packageJsonPath =
-      path.join(
-        config.targetDir,
-        "package.json"
-      );
-
-
-    if (
-      !await fs.pathExists(packageJsonPath)
-    ) {
-
-      spinner.warn(
-        "package.json not found"
-      );
-
+    if (!(await fs.pathExists(packageJsonPath))) {
+      spinner.warn("package.json not found");
       return;
-
     }
 
+    const packageJson = await fs.readJson(packageJsonPath);
 
+    packageJson.name = normalizePackageName(
+      config.projectName
+    );
 
-    const packageJson =
-      await fs.readJson(
-        packageJsonPath
-      );
-
-
-
-    packageJson.name =
-      normalizePackageName(
-        config.projectName
-      );
-
-
-
-    if (
-      config.dependencies
-    ) {
-
+    if (config.dependencies) {
       packageJson.dependencies = {
-
         ...packageJson.dependencies,
-
         ...config.dependencies
-
       };
-
     }
 
-
-
-    if (
-      config.devDependencies
-    ) {
-
+    if (config.devDependencies) {
       packageJson.devDependencies = {
-
         ...packageJson.devDependencies,
-
         ...config.devDependencies
-
       };
-
-    }
-
-    if (
-      config.devDependencies
-    ) {
-
-      packageJson.devDependencies = {
-
-        ...packageJson.devDependencies,
-
-        ...config.devDependencies
-
-      };
-
     }
 
     await fs.writeJson(
@@ -101,35 +44,17 @@ export async function updatePackageJson(config) {
       }
     );
 
-
-    spinner.succeed(
-      "package.json updated"
-    );
-
-
+    spinner.succeed("package.json updated");
   } catch (error) {
-
-
-    spinner.fail(
-      "Failed to update package.json"
-    );
-
-
+    spinner.fail("Failed to update package.json");
     throw error;
-
   }
-
 }
 
-
-
-function normalizePackageName(
-  name
-) {
-
+function normalizePackageName(name) {
   return name
     .trim()
     .toLowerCase()
     .replace(/\s+/g, "-");
-
 }
+
